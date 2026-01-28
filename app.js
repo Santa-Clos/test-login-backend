523fd48f4538c479d8cd9e96134441c755eab03b
Santa-Clos <xavi.arcosde@gmail.com>
2026-01-28 23:11:41 +0100

    Movie html display with servor, CORS error handling and general API REST development

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const movies = require('./movies.json')
 const express = require('express')
+const crypto = require('node:crypto')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.disable('x-powered-by')
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hola mundo' })
 })
 
 app.get('/movies', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -24,6 +29,43 @@ app.get('/movies/:id', (req, res) => {
   const movie = movies.find(movie => movie.id === id)
   if (movie) return res.json(movie)
   else return res.status(400).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const result = validatePartialMovie(req.body)
+
+  if (result.error) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' })
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+  return res.json(updateMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
