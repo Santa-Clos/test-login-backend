82a2f577dafaba565dca9a60060d2fce61a6c8d8
Santa-Clos <xavi.arcosde@gmail.com>
2026-01-29 15:30:53 +0100

    Added some MVC clean features

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import express, { json } from 'express'
-import { moviesRouter } from './routes/movies'
-import { corsMiddleware } from './middlewares/cors'
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 app.disable('x-powered-by')
-
 app.use(json())
 app.use(corsMiddleware())
 
-app.use('/movies', moviesRouter)
+app.use('/movies', moviesRouter) // /movies
 
 const PORT = process.env.PORT ?? 1234
 
--- routes/movies.js
+++ routes/movies.js
@@ -1,71 +1,11 @@
 import { Router } from 'express'
-import { readJSON } from '../utils/jsonimporter'
-import { randomUUID } from 'node:crypto'
-import { validateMovie, validatePartialMovie } from './schemas/movies.js'
-const movies = readJSON('./movies.json')
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-  const { genre } = req.query
-  if (genre) {
-    const filteredMovies = movies.filter(
-      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
-  }
-  return res.json(movies)
-})
+moviesRouter.get('/', MovieController.getAll)
+moviesRouter.get('/:id', MovieController.getById)
 
-moviesRouter.get('/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find(movie => movie.id === id)
-  if (movie) return res.json(movie)
-  else return res.status(400).json({ message: 'Movie not found' })
-})
-
-moviesRouter.post('/', (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newMovie = {
-    id: randomUUID(),
-    ...result.data
-  }
-
-  movies.push(newMovie)
-  res.status(201).json(newMovie)
-})
-
-moviesRouter.delete('/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movieIndex.id === id)
-  if (movieIndex === -1) return res.status(404).json({ message: 'Movie Not Found' })
-  movies.splice(movieIndex, 1)
-  return res.json({ message: 'Movie deleted' })
-})
-
-moviesRouter.patch('/:id', (req, res) => {
-  const { id } = req.params
-  const result = validatePartialMovie(req.body)
-
-  if (result.error) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' })
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-  }
-
-  movies[movieIndex] = updateMovie
-  return res.json(updateMovie)
-})
+moviesRouter.post('/', MovieController.create)
+moviesRouter.delete('/:id', MovieController.delete)
+moviesRouter.patch('/:id', MovieController.update)
